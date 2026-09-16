a0d7fa1b2ecee2f95e379c69b2f6b698aac1bba2
LitelleGalaxy <ellesimeon.work@gmail.com>
2026-09-17 05:13:34 +0930

    Format supabase.js for improved readability

--- supabase.js
+++ supabase.js
@@ -1,16 +1,16 @@
 // supabase.js
 
 const SUPABASE_URL =
-  "https://ullucytrbqwibkgklvab.supabase.co";
+    "https://ullucytrbqwibkgklvab.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_rKJtkfPqwKIuEpgL4TRF-g_2IN_0xyc";
+    "sb_publishable_rKJtkfPqwKIuEpgL4TRF-g_2IN_0xyc";
 
 const supabaseClient =
-  window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_PUBLISHABLE_KEY
-  );
+    window.supabase.createClient(
+        SUPABASE_URL,
+        SUPABASE_PUBLISHABLE_KEY
+    );
 
 
 /* =========================================================
